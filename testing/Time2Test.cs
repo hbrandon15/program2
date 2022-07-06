fc61acebaef11b8a3be692b9846a57276ab0e321
Brandon J. Hernandez <hernandezbrandon17@gmail.com>
2022-07-06 09:45:56 -0500

    Creating class library with test
    Creating class library with test

---
+++ testing/Time2Test.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+namespace program2
+{
+
+    public class Time2Test
+    {
+        static void Main()
+        {
+            var t1 = new Time2();
+            var t2 = new Time2(2);
+            var t3 = new Time2(21, 34);
+            var t4 = new Time2(12, 25, 42);
+            var t5 = new Time2(t4);
+
+
+
+
+
+
+            Console.WriteLine("Constructed with:\n");
+            Console.WriteLine("t1: all arguments defaulted");
+            Console.WriteLine($" {t1.ToUniversalString()}"); // 00:00:00
+            Console.WriteLine($" {t1.ToString()}\n"); // 12:00:00 AM
+
+            Console.WriteLine(
+            "t2: hour specified; minute and second defaulted");
+            Console.WriteLine($" {t2.ToUniversalString()}"); // 02:00:00
+            Console.WriteLine($" {t2.ToString()}\n"); // 2:00:00 AM
+
+            Console.WriteLine(
+            "t3: hour and minute specified; second defaulted");
+            Console.WriteLine($" {t3.ToUniversalString()}"); // 21:34:00
+            Console.WriteLine($" {t3.ToString()}\n"); // 9:34:00 PM
+
+            Console.WriteLine("t4: hour, minute and second specified");
+            Console.WriteLine($" {t4.ToUniversalString()}"); // 12:25:42
+            Console.WriteLine($" {t4.ToString()}\n"); // 12:25:42 PM
+
+            Console.WriteLine("t5: Time2 object t4 specified");
+            Console.WriteLine($" {t5.ToUniversalString()}"); // 12:25:42
+            Console.WriteLine($" {t5.ToString()}"); // 12:25:42 PM
+
+            // attempt to initialize t6 with invalid values
+            try
+            {
+
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("\nException while initializing t6:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
