e7b5a3d48316d72ffb53cf4ff0b82e04cad98187
Brandon Hernandez <hernandezbrandon17@gmail.com>
2022-07-09 15:14:11 -0500

    Created new console app for test
    Created new console app for test

---
+++ program2Test2/Time2TestLINQ.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+namespace program2
+{
+
+    public class Time2TestLINQ
+    {
+        static void Main()
+        {
+            do
+                {
+                Console.WriteLine("Which type of object do you wish to enter?");
+                Console.WriteLine("1 - Time2");
+                Console.WriteLine("2 - Time2sw");
+                Console.WriteLine("3 - Stop entering data");
+                
+
+            } while (true);
+
+
+
+
+
+
+            
+        }
+    }
+}
